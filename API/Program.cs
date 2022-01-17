c58a8b876e09f9656268d73acc7e6901e88423c0
fellipe coelho <fellipefcoelho@gmail.com>
2022-01-17 15:17:23 -0300

    fronted-backbone

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
@@ -33,6 +34,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(opt => opt.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000")
+);
 
 app.UseAuthorization();
 
---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+
+
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+      fetch('https://localhost:7293/api/Products')
+        .then(response => response.json())
+        .then(data => setProducts(data));
+    }, [])
+  
+    return (
+        <>
+            <ProductList
+                products={products}
+            />
+        </>
+    )
+}
